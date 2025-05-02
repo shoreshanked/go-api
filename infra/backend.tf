b19bbc768b0113c9d32b0608c77c3dab34b6856f
shoreshanked <williamshore11@hotmail.com>
2025-05-02 13:44:02 +0100

    Added remote backend

--- infra/backend.tf
+++ infra/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "terraform-dev-state-bucket-229418028078"
     key            = "dev/infra/terraform.tfstate"
-    region         = var.aws_region
+    region         = "eu-west-1"
     dynamodb_table = "terraform-dev-state-lock-229418028078"
     encrypt        = true
   }
